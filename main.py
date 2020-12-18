56431ba9cffc5de9b1c57feea7fc5ad691017c07
Shamnas <shamnastv@gmail.com>
2020-12-18 11:40:38 +0530

    update

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def pass_data_iteratively(model, x, initial, batch_size=128):
     return torch.cat(outputs), torch.cat(alphas), torch.cat(targets)
 
 
-def train_init(args, alphas_list, k, device):
+def main_init(args, alphas_list, k, device):
     max_test_acc = 0
     num_clasees = 3
     dataset, word_to_id, word_list, word_embeddings = load_data(args.dataset_name, alphas_list, True)
@@ -49,61 +49,67 @@ def train_init(args, alphas_list, k, device):
     model = Model(args, num_clasees, word_embeddings, device).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=.9)
-    # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
 
     alphas_to_save = None
     batch_size = args.batch_size
     for epoch in range(1, args.epochs + 1):
-        model.train()
-        train_idx = np.random.permutation(train_size)
-        loss_accum = 0
-        for i in range(0, train_size, batch_size):
-            idx = train_idx[i: i + batch_size]
-            output, alpha, target, a_mask, a_value = model([train_data[i] for i in idx], initial=True)
-            loss = criterion(output, target)
+        train_init(epoch, model, optimizer, train_data, train_size, batch_size)
 
-            optimizer.zero_grad()
-            loss.backward()
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
-            optimizer.step()
-
-            loss = loss.detach().cpu().numpy()
-            loss_accum += loss
-
-        print('epoch :', epoch, 'loss :', loss_accum, flush=True)
-
-        model.eval()
-        outputs_train, alphas_train, targets_train = pass_data_iteratively(model, train_data, batch_size=128,
-                                                                           initial=True)
-        pred_train = outputs_train.max(1, keepdim=True)[1]
-        correct = pred_train.eq(targets_train.view_as(pred_train)).sum().cpu().item()
-        train_acc = correct / float(len(targets_train))
-
-        outputs_test, alpha_test, targets_test = pass_data_iteratively(model, test_data, batch_size=128, initial=True)
-        pred_test = outputs_test.max(1, keepdim=True)[1]
-        correct = pred_test.eq(targets_test.view_as(pred_test)).sum().cpu().item()
-        test_acc = correct / float(len(targets_test))
-
-        if test_acc > max_test_acc:
-            max_test_acc = test_acc
-            pred_train = pred_train.view_as(targets_train)
-            alphas_to_save = alphas_train.detach().cpu().numpy()
-            for ind in range(len(pred_train)):
-                if pred_train[ind] != targets_train[ind]:
-                    alphas_to_save[ind] = -alphas_to_save[ind]
-
-        print('initial', k, 'epoch :', epoch, 'accuracy train :', train_acc, 'test :', test_acc, flush=True)
+        alphas_to_save, max_test_acc = test_init(epoch, model, scheduler, train_data, test_data, alphas_to_save,
+                                                 max_test_acc, k)
         print('')
     print('max test accuracy : ', max_test_acc)
     print('=' * 100)
 
-    # if alphas_to_save is not None:
-    #     np.savetxt(alpha_files[k], alphas_to_save)
     alphas_list.append(alphas_to_save)
     return alphas_list
 
 
-def train_final(args, alphas_list, k, device):
+def test_init(epoch, model, scheduler, train_data, test_data, alphas_to_save, max_test_acc, k):
+    model.eval()
+    outputs_train, alphas_train, targets_train = pass_data_iteratively(model, train_data, batch_size=128,
+                                                                       initial=True)
+    pred_train = outputs_train.max(1, keepdim=True)[1]
+    correct = pred_train.eq(targets_train.view_as(pred_train)).sum().cpu().item()
+    train_acc = correct / float(len(targets_train))
+    outputs_test, alpha_test, targets_test = pass_data_iteratively(model, test_data, batch_size=128, initial=True)
+    pred_test = outputs_test.max(1, keepdim=True)[1]
+    correct = pred_test.eq(targets_test.view_as(pred_test)).sum().cpu().item()
+    test_acc = correct / float(len(targets_test))
+    if test_acc > max_test_acc:
+        max_test_acc = test_acc
+        pred_train = pred_train.view_as(targets_train)
+        alphas_to_save = alphas_train.detach().cpu().numpy()
+        for ind in range(len(pred_train)):
+            if pred_train[ind] != targets_train[ind]:
+                alphas_to_save[ind] = -alphas_to_save[ind]
+    else:
+        scheduler.step()
+    print('initial', k, 'epoch :', epoch, 'accuracy train :', train_acc, 'test :', test_acc, flush=True)
+    return alphas_to_save, max_test_acc
+
+
+def train_init(epoch, model, optimizer, train_data, train_size, batch_size):
+    model.train()
+    train_idx = np.random.permutation(train_size)
+    loss_accum = 0
+    for i in range(0, train_size, batch_size):
+        idx = train_idx[i: i + batch_size]
+        output, alpha, target, a_mask, a_value = model([train_data[i] for i in idx], initial=True)
+        loss = criterion(output, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
+        optimizer.step()
+
+        loss = loss.detach().cpu().numpy()
+        loss_accum += loss
+    print('epoch :', epoch, 'loss :', loss_accum, flush=True)
+
+
+def main_final(args, alphas_list, k, device):
     max_test_acc = 0
     num_clasees = 3
     dataset, word_to_id, word_list, word_embeddings = load_data(args.dataset_name, alphas_list, False)
@@ -118,45 +124,13 @@ def train_final(args, alphas_list, k, device):
     model = Model(args, num_clasees, word_embeddings, device).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=.9)
-    # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
 
     batch_size = args.batch_size
     for epoch in range(1, args.epochs + 1):
-        model.train()
-        train_idx = np.random.permutation(train_size)
-        loss_accum = 0
-        for i in range(0, train_size, batch_size):
-            idx = train_idx[i: i + batch_size]
-            output, alpha, target, a_mask, a_value = model([train_data[i] for i in idx], initial=False)
-            loss1 = criterion(output, target)
-            loss2 = mse_loss(alpha * a_mask, a_value)
-            loss = loss1 + args.beta * loss2
+        train_final(epoch, args, model, optimizer, train_data, train_size, batch_size)
 
-            optimizer.zero_grad()
-            loss.backward()
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
-            optimizer.step()
-
-            loss = loss.detach().cpu().numpy()
-            loss_accum += loss
-        print('epoch :', epoch, 'loss :', loss_accum, flush=True)
-
-        model.eval()
-        outputs_train, alphas_train, targets_train = pass_data_iteratively(model, train_data, batch_size=128,
-                                                                           initial=True)
-        pred_train = outputs_train.max(1, keepdim=True)[1]
-        correct = pred_train.eq(targets_train.view_as(pred_train)).sum().cpu().item()
-        train_acc = correct / float(len(targets_train))
-
-        outputs_test, alpha_test, targets_test = pass_data_iteratively(model, test_data, batch_size=128, initial=True)
-        pred_test = outputs_test.max(1, keepdim=True)[1]
-        correct = pred_test.eq(targets_test.view_as(pred_test)).sum().cpu().item()
-        test_acc = correct / float(len(targets_test))
-
-        if test_acc > max_test_acc:
-            max_test_acc = test_acc
-
-        print('final', k, 'epoch :', epoch, 'accuracy train :', train_acc, 'test :', test_acc, flush=True)
+        max_test_acc = test_final(epoch, model, scheduler, train_data, test_data, max_test_acc, k)
 
         print('')
 
@@ -164,6 +138,46 @@ def train_final(args, alphas_list, k, device):
     print('=' * 100)
 
     return alphas_list
+
+
+def test_final(epoch, model, scheduler, train_data, test_data, max_test_acc, k):
+    model.eval()
+    outputs_train, alphas_train, targets_train = pass_data_iteratively(model, train_data, batch_size=128,
+                                                                       initial=True)
+    pred_train = outputs_train.max(1, keepdim=True)[1]
+    correct = pred_train.eq(targets_train.view_as(pred_train)).sum().cpu().item()
+    train_acc = correct / float(len(targets_train))
+    outputs_test, alpha_test, targets_test = pass_data_iteratively(model, test_data, batch_size=128, initial=True)
+    pred_test = outputs_test.max(1, keepdim=True)[1]
+    correct = pred_test.eq(targets_test.view_as(pred_test)).sum().cpu().item()
+    test_acc = correct / float(len(targets_test))
+    if test_acc > max_test_acc:
+        max_test_acc = test_acc
+    else:
+        scheduler.step()
+    print('final', k, 'epoch :', epoch, 'accuracy train :', train_acc, 'test :', test_acc, flush=True)
+    return max_test_acc
+
+
+def train_final(epoch, args, model, optimizer, train_data, train_size, batch_size):
+    model.train()
+    train_idx = np.random.permutation(train_size)
+    loss_accum = 0
+    for i in range(0, train_size, batch_size):
+        idx = train_idx[i: i + batch_size]
+        output, alpha, target, a_mask, a_value = model([train_data[i] for i in idx], initial=False)
+        loss1 = criterion(output, target)
+        loss2 = mse_loss(alpha * a_mask, a_value)
+        loss = loss1 + args.beta * loss2
+
+        optimizer.zero_grad()
+        loss.backward()
+        # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
+        optimizer.step()
+
+        loss = loss.detach().cpu().numpy()
+        loss_accum += loss
+    print('epoch :', epoch, 'loss :', loss_accum, flush=True)
 
 
 def main():
@@ -199,12 +213,12 @@ def main():
     max_k = 5
     alphas_list = []
     for k in range(max_k):
-        alphas_list = train_init(args, alphas_list, k, device)
+        alphas_list = main_init(args, alphas_list, k, device)
 
     alphas_list_new = []
     for k in range(max_k):
         alphas_list_new.append(alphas_list[k])
-        alphas_list = train_final(args, alphas_list, k, device)
+        alphas_list = main_final(args, alphas_list, k, device)
 
     print('=' * 150)
 
