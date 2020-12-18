7cd609be3ce0205cb38b299ddb30fa483e4a86f4
Shamnas <shamnastv@gmail.com>
2020-12-18 23:31:51 +0530

    update

--- laptop.sh
+++ laptop.sh
@@ -6,4 +6,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
+python3 main.py --hidden_dim 50 --lr .001 --batch_size 64 --dataset_name 14semeval_laptop
+python3 main.py --hidden_dim 50 --lr .0005 --batch_size 64 --dataset_name 14semeval_laptop
+python3 main.py --hidden_dim 50 --lr .003 --batch_size 64 --dataset_name 14semeval_laptop
+python3 main.py --hidden_dim 100 --lr .001 --batch_size 64 --dataset_name 14semeval_laptop
+python3 main.py --hidden_dim 300 --lr .001 --batch_size 64 --dataset_name 14semeval_laptop
 python3 main.py --hidden_dim 50 --lr .001 --batch_size 25 --dataset_name 14semeval_laptop
+
